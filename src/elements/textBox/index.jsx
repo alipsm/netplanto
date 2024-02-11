eb871f9cd82f72b650f527ada4b4e8e8098f361e
alipsm <stooormix@gmail.com>
2024-02-11 23:58:11 +0330

    add name for textBox element

--- src/elements/textBox/index.jsx
+++ src/elements/textBox/index.jsx
@@ -5,12 +5,12 @@ TextBox.defaultProps = {
   getValue: () => "",
   type: "text"
 }
-export default function TextBox({ placeholder, onchange, getValue, type, success }) {
+export default function TextBox({name, placeholder, onchange, getValue, type, success }) {
   const [inputType, setInputType] = useState(type);
   return (
     <div className='relative w- max'>
       {type === "password" && <BiSolidHide onClick={() => setInputType(inputType === "password" ? "text" : "password")} className="w-8 h-8 absolute top-4 right-2 cursor-pointer" color='#9B9B9A' />}
-      <input type={inputType} placeholder={placeholder} onChange={e => getValue(e.target.value)} className='shadow-md shadow-[#00000052] w-full py-3 px-2 sm:px-3 sm:py-4 border-none rounded-2xl outline-none text-sm sm:text-2xl max-w-96' />
+      <input name={name} type={inputType} placeholder={placeholder} onChange={e => getValue(e.target.value)} className='shadow-md shadow-[#00000052] w-full py-3 px-2 sm:px-3 sm:py-4 border-none rounded-2xl outline-none text-sm sm:text-2xl max-w-96' />
       {success && <IoMdCheckmark className="w-10 h-10 absolute top-3 -right-10" color="#1C7933" />}
     </div>
   )

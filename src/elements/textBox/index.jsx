e814ed2cff8476a79e86d3ea14c22bea690f80de
alipsm <stooormix@gmail.com>
2024-02-18 17:30:32 +0330

    edit green heck mark position

--- src/elements/textBox/index.jsx
+++ src/elements/textBox/index.jsx
@@ -11,7 +11,7 @@ export default function TextBox({name, placeholder, onchange, getValue, type, su
     <div className='relative w- max'>
       {type === "password" && <BiSolidHide onClick={() => setInputType(inputType === "password" ? "text" : "password")} className="w-8 h-8 absolute top-4 right-2 cursor-pointer" color='#9B9B9A' />}
       <input name={name} value={value} type={inputType} placeholder={placeholder} onChange={e => getValue(e.target.value)} className='shadow-md shadow-[#00000052] w-full py-3 px-2 sm:px-3 sm:py-4 border-none rounded-2xl outline-none text-sm sm:text-2xl max-w-96' />
-      {success && <IoMdCheckmark className="w-10 h-10 absolute top-3 -right-10" color="#1C7933" />}
+      {success && <IoMdCheckmark className="flex justify-center items-center w-10 h-full absolute top-0 right-2" color="#1C7933" />}
     </div>
   )
 }

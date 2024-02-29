feff4d9641596a5ad163dcacef7af764571cff7c
alipsm <stooormix@gmail.com>
2024-02-29 23:13:47 +0330

    edit text box show pass ico

--- src/elements/textBox/index.jsx
+++ src/elements/textBox/index.jsx
@@ -9,7 +9,7 @@ export default function TextBox({name, placeholder, onchange, getValue, type, su
   const [inputType, setInputType] = useState(type);
   return (
     <div className='relative w- max'>
-      {type === "password" && <BiSolidHide onClick={() => setInputType(inputType === "password" ? "text" : "password")} className="w-8 h-8 absolute top-4 right-2 cursor-pointer" color='#9B9B9A' />}
+      {type === "password" && <BiSolidHide onClick={() => setInputType(inputType === "password" ? "text" : "password")} className=" w-6 h-6 sm:w-8 sm:h-8 absolute top-3 sm:top-4 right-2 cursor-pointer" color='#9B9B9A' />}
       <input name={name} value={value} type={inputType} placeholder={placeholder} onChange={e => getValue(e.target.value)} className='shadow-md shadow-[#00000052] w-full py-3 px-2 sm:px-3 sm:py-4 border-none rounded-2xl outline-none text-sm sm:text-2xl max-w-96' />
       {success && <IoMdCheckmark className="flex justify-center items-center w-10 h-full absolute top-0 right-2" color="#1C7933" />}
     </div>

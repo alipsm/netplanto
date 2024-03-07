e74c4523233bf40ecab67ae1cad28bfdebbc9ccd
alipsm <stooormix@gmail.com>
2024-03-08 01:31:02 +0330

    set forget pass api

--- src/pages/accountOperation/forgetPassword/index.jsx
+++ src/pages/accountOperation/forgetPassword/index.jsx
@@ -2,18 +2,46 @@ import React, { useState } from 'react'
 import TextBox from '../../../elements/textBox'
 import Button from '../../../elements/button'
 import { Link } from 'react-router-dom'
+import { convertFormDataToJson } from '../../../components/utils/convertFormDataToJson'
+import useApi from '../../../hook/useApi'
+import { toast } from 'react-toastify'
+import useFormValidation from '../../../hook/useFormValidation'
 
 export default function ForgotPassword() {
     const [showBanner, setShowBanner] = useState(false)
+
+    const { getValidation } = useFormValidation();
+    const { post } = useApi()
+
+    const handleSubmitForgetPasswordForm = async (e) => {
+        e.preventDefault()
+        const { status, message, field } = getValidation(e);
+        if (status) {
+            try {
+                const jsonData = convertFormDataToJson(e)
+                const data = await post("/user/forgetPassword/",jsonData)
+                console.log('data', data)
+                localStorage.setItem("token",data.token)
+                setShowBanner(true)
+                toast.success(data?.message)
+            } catch (error) {
+                toast.error(error.message)
+            } finally {
+            }
+        } else {
+            toast.error(message)
+        }
+    }
+
     return (
         <div className='f lex flex-col items-center justify-evenly relative w-full h-full z-10 sm:w-4/6'>
             {!showBanner ?(
-            <div className=' flex flex-col justify-center items-center gap-5 justify-self-start m-auto relative w-4/6  h-3/6 sm:h-4/6'>
-                <TextBox placeholder={"Email & Username"} type={"text"} />
+            <form onSubmit={handleSubmitForgetPasswordForm} className=' flex flex-col justify-center items-center gap-5 justify-self-start m-auto relative w-4/6  h-3/6 sm:h-4/6'>
+                <TextBox placeholder={"Phone"} name={"phone"} type={"number"} />
                 <div className=' w-36'>
                     <Button text={"Sent"} />
                 </div>
-            </div>
+            </form>
 
 
             ): (
@@ -23,7 +51,7 @@ export default function ForgotPassword() {
                         a password reset message was sent to your email address.
                         please click the link in that message to reset your password.
                     </p>
-                    <Link to={"/accountOperation/login"} className='underline absolute left-0 inline-block w-full text-center bottom-10 m-auto'>Ok</Link>
+                    <Link to={"/accountOperation/resetPassword"} className='underline absolute left-0 inline-block w-full text-center bottom-10 m-auto'>Ok</Link>
                     </div>
                 </div>
             )}
